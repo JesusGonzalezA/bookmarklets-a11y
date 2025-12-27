7dc4f101c7558331a3131a7c282a2e99c5e7fa1c
Jesús González Álvarez <jesusgal@ext.inditex.com>
2025-12-27 13:59:13 +0100

    First commit

---
+++ build.js
@@ -0,0 +1,17 @@
+import fs from 'fs'
+import path from 'path'
+import { execSync } from 'child_process'
+
+const srcDir = path.resolve('src')
+
+const bookmarklets = fs
+  .readdirSync(srcDir, { withFileTypes: true })
+  .filter(d => d.isDirectory())
+  .map(d => d.name)
+
+for (const name of bookmarklets) {
+  console.log(`\nBuilding bookmarklet: ${name}`)
+  execSync(`npx vite build --mode ${name}`, {
+    stdio: 'inherit'
+  })
+}
---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig(({ mode }) => {
+  return {
+    build: {
+      emptyOutDir: false,
+      minify: false,
+      rollupOptions: {
+        input: resolve(__dirname, `src/${mode}/index.js`),
+        output: {
+          format: 'iife',
+          name: 'bookmarklet',
+          entryFileNames: `${mode}.js`,
+          inlineDynamicImports: true,
+          banner: 'javascript:'
+        }
+      }
+    }
+  }
+})
